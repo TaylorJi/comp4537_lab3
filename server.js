ea799ae38c2d6cc381d7e101004002aed4d785d3
Siwoon Lim <59671717+SWL-01@users.noreply.github.com>
2023-09-28 17:01:33 -0700

    added configfile

--- server.js
+++ server.js
@@ -1,17 +1,16 @@
-const http = require('http');
-const url = require('url');
- 
-const hostname = '127.0.0.1';
-const port = 3000;
-const currentDate = require('./module/utils.js').getDate();
-const server = http.createServer((req, res) => {
-    const parsedUrl = url.parse(req.url, true);
+import { createServer } from "http";
+import { parse } from "url";
 
-    if (parsedUrl.pathname === '/getDate/') {
-        const name = parsedUrl.query.name || "Guest";
-        // const currentTime = new Date().toLocaleTimeString();
+const currentDate = require("./module/utils.js").getDate();
 
-        const responseMessage = `
+createServer((req, res) => {
+    const parsedUrl = parse(req.url, true);
+
+    if (parsedUrl.pathname === "/getDate/") {
+      const name = parsedUrl.query.name || "Guest";
+      // const currentTime = new Date().toLocaleTimeString();
+
+      const responseMessage = `
             <span style="color: blue;">
                 <strong>
                 Hello, ${name}! What a beautiful day. Server current date and time is ${currentDate}.
@@ -19,14 +18,11 @@ const server = http.createServer((req, res) => {
             </span>
         `;
 
-        res.writeHead(200, { 'Content-Type': 'text/html' }); // head
-        res.end(responseMessage); // body
+      res.writeHead(200, { "Content-Type": "text/html" }); // head
+      res.end(responseMessage); // body
     } else {
-        res.writeHead(404, { 'Content-Type': 'text/plain' });
-        res.end("404 Not Found");
+      res.writeHead(404, { "Content-Type": "text/plain" });
+      res.end("404 Not Found");
     }
-});
- 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+  })
+  .listen(8888);
